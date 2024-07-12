1c22f829141ab0a4f7f8a6e2558f87202f4ec7cc
mackingsley <kingsleyudoma2018@gmail.com>
2024-07-12 15:43:12 +0100

    updates

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -94,10 +94,10 @@ const handlers = {
     console.log('Handler trade.started called with payload:', payload); // Logging
     await tradesHandler.markAsStarted(payload.trade_hash);
     const response = await paxfulApi.invoke('/paxful/v1/trade/get', { trade_hash: payload.trade_hash });
-    await paxfulApi.invoke('/paxful/v1/trade-chat/post', {
-      trade_hash: tradeHash,
-      message: `Good Day Boss, pls Drop account`
-  });
+  //   await paxfulApi.invoke('/paxful/v1/trade-chat/post', {
+  //     trade_hash: tradeHash,
+  //     message: `Good Day Boss, pls Drop account`
+  // });
 
     console.log(response);
     console.log('Trade started Invocation');
