532ac1d39f731050b5f3b0ee97c63952e911f8b6
mackingsley <kingsleyudoma2018@gmail.com>
2024-08-26 15:10:52 +0100

    updated

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -483,4 +483,39 @@ router.post('/paxful/webhook', async (req, res) => {
 
 
 
+/////Rates 
+
+router.post('/paxful/paxful/rates', async (req, res) => {
+
+  const hash = req.body.hash;
+  const paxfulApi = req.context.services.paxfulApi;
+  try {
+   const theres = await paxfulApi.invoke('/paxful/v1/currency/btc?response=text', {});
+
+      res.json({ status: 'success', message: 'Message sent successfully.','Done?':theres });
+      console.log(theres);
+  } catch (error) {
+      console.error('Error sending chat message:', error);
+      res.status(500).json({ status: 'error', message: 'Failed to send message.','error':error });
+  }
+});
+
+
+router.post('/paxful/binance/rates', async (req, res) => {
+  try {
+    // Make a request to the Binance API
+    const response = await axios.get('https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+
+    // Extract the price from the response
+    const price = response.data.price;
+
+    // Return the price in the response
+    res.json({ price });
+  } catch (error) {
+    // Handle any errors
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch the price from Binance' });
+  }
+});
+
 module.exports = router;
