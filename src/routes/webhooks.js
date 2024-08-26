4167ddb61a5cb5d59dbb043dc2f520eb26f85029
mackingsley <kingsleyudoma2018@gmail.com>
2024-08-26 15:28:01 +0100

    update

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -493,7 +493,7 @@ router.post('/paxful/paxful/rates', async (req, res) => {
   try {
    const theres = await paxfulApi.invoke('/paxful/v1/currency/btc?response=text', {});
 
-      res.json({theres});
+      res.json({ price:theres });
       console.log(theres);
   } catch (error) {
       console.error('Error sending chat message:', error);
